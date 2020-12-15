95f7bd1a820894c5231c07a02a0f9a78acb9bd20
Crystallee1104 <71081865+Crystallee1104@users.noreply.github.com>
2020-12-15 10:45:52 +0000

    Upload for循环 python file
    第一次GitHub上传python学习file

---
+++ for循环.py
@@ -0,0 +1,51 @@
+# tags='我是一个中国人'
+# for item in tags:
+#     print(item)
+#     pass
+
+# range(1,100)
+# # print(type(range))
+# sum=0
+# for data in range(1,101):
+#     sum+=data
+#     print(data,end=" ")
+#     pass
+
+# print('sum=%d'%sum)
+
+# for data in range(50,201):
+#     if data%2==0:
+#         print('%d是偶数'%data)
+#         pass
+#     else:
+#         print('%d是奇数'%data)
+# sum=0
+# for item in range(1,51):
+#     if sum>100:
+#         print('循环执行到%d就结束'%item)
+#         break
+#         pass
+#     sum+=item
+#     pass
+
+# print('sum=%d'%sum)
+
+# for item in range(1,100):
+#     if item%2==0:
+#         continue
+#         print('continue后面执行')
+#         pass
+#     print(item)
+#     pass
+
+# for item in "I love python":
+#     if item=='o':
+#         continue
+#     print(item,end='')
+
+index=1
+while index<=100:
+    if index>20:
+        break
+    print(index)
+    index+=1
